2d4fa5c367e200f71c628e42bf5d5ae91ed7e471
Parvez Khan <99182155+parvezzkhan@users.noreply.github.com>
2023-10-20 12:58:54 +0530

    Update models.py

--- naats/models.py
+++ naats/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from djrichtextfield.models import RichTextField
 
 
 class Naat(models.Model):
     tittle = models.CharField(max_length=100)
-    lyrics = models.TextField()
+    lyrics = RichTextField()
 
 
     def __str__(self):

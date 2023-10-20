be292405dd914e509c048b6242adc72b3575ca60
Parvez Khan <99182155+parvezzkhan@users.noreply.github.com>
2023-10-20 12:42:01 +0530

    Update models.py

--- naats/models.py
+++ naats/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from ckeditor.fields import RichTextField
+
 
 class Naat(models.Model):
     tittle = models.CharField(max_length=100)
-    lyrics = RichTextField()
+    lyrics = models.TextField()
+
 
     def __str__(self):
         return self.tittle
